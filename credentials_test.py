cf2f1aad1935cb8ac578be3fc8422be07bc04247
NessaAz <vanessa.azenwa@gmail.com>
2022-04-25 13:57:53 +0300

    add definition to the test save multiple user credentials

--- credentials_test.py
+++ credentials_test.py
@@ -12,10 +12,10 @@ class TestCredentials(unittest.TestCase):
         test_init test case to test if the object is initialized properly
         '''
 
-        self.assertEqual(self.new_credentials.credentials_name,"Vanessa Az")
-        self.assertEqual(self.new_credentials.credaccount_name,"Vanessa Azenwa")
-        self.assertEqual(self.new_credentials.password,"CooL99$")
-        self.assertEqual(self.new_credentials.email,"vanessa.azenwa@gmail.com")
+        self.assertEqual(self.new_credentials.credentials_name,"Vanessa Aze")
+        self.assertEqual(self.new_credentials.credaccount_name,"Vanessa Azen")
+        self.assertEqual(self.new_credentials.password,"CoL99$")
+        self.assertEqual(self.new_credentials.email,"vanessamwikali@gmail.com")
     
     def test_save_credentials(self):
         '''
@@ -30,6 +30,16 @@ class TestCredentials(unittest.TestCase):
             tearDown method that does clean up after each test case has run.
             '''
             Credentials.credentials_list = []    
+            
+    def test_save_multiple_credentials(self):
+            '''
+            test_save_multiple_account to check if we can save multiple account
+            objects to our account_list
+            '''
+            self.new_credentials.save_credentials()
+            test_credentials = Credentials("Test1","user1","66778899","test1@user.com") # new user
+            test_credentials.save_credentials()
+            self.assertEqual(len(Credentials.credentials_list),2)        
     
     
 
