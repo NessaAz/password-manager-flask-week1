05f54674ea0bd4cfa86140e68e4a47ad5dc6c136
NessaAz <vanessa.azenwa@gmail.com>
2022-04-25 06:08:24 +0300

    define the credentials list

--- credentials.py
+++ credentials.py
@@ -2,4 +2,11 @@ class Credentials:
     '''
     class that generates new instance of user credentials
     '''
+    
     credentials_list = [] #empty credentials list
+    
+    def __init__(self,credentials_name,account_name,password,email):
+        self.credentials_name = credentials_name
+        self.account_name = account_name
+        self.password = password
+        self.email = email
