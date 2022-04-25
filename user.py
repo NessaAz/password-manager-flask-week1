faec2d295af2d41fe9add00ea216a876fd2efa2f
NessaAz <vanessa.azenwa@gmail.com>
2022-04-25 06:08:44 +0300

    fefine the user list

--- user.py
+++ user.py
@@ -5,3 +5,9 @@ class User:
     
     user_list = []  #empty user's list
     
+    def __init__(self,account_name,user_name,password,email):
+        self.account_name = account_name
+        self.user_name = user_name
+        self.password = password
+        self.email = email
+    
