8e5b7f413c3eebf140b34aa3b2c1bc48fa37bd08
NessaAz <vanessa.azenwa@gmail.com>
2022-04-25 09:23:43 +0300

    define the test init test that tests if new credentials object is initialized properly

--- credentials_test.py
+++ credentials_test.py
@@ -1,6 +1,3 @@
-import imp
-
-
 import unittest #importing the unittest module
 from credentials import Credentials #importing the credentials class
 
@@ -9,5 +6,16 @@ class TestCredentials(unittest.TestCase):
     def setUp(self):
        
         self.new_credentials = Credentials("Vanessa","Azenwa","CooL99$","vanessa.azenwa@gmail.com") # create Cresentials object
+        
+    def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
+
+        self.assertEqual(self.new_credentials.credentials_name,"Vanessa Az")
+        self.assertEqual(self.new_credentials.account_name,"Vanessa Azenwa")
+        self.assertEqual(self.new_credentials.password,"CooL99$")
+        self.assertEqual(self.new_credentials.email,"vanessa.azenwa@gmail.com")
+    
         
 
