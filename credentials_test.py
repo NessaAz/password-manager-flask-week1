aae156fed1cddb25d73df9a186c8cea576867005
NessaAz <vanessa.azenwa@gmail.com>
2022-04-25 06:23:37 +0300

    add new user & credentials objects in test files

--- credentials_test.py
+++ credentials_test.py
@@ -4,3 +4,10 @@ import imp
 import unittest #importing the unittest module
 from credentials import Credentials #importing the credentials class
 
+
+class TestCredentials(unittest.TestCase):
+    def setUp(self):
+       
+        self.new_credentials = Credentials("Vanessa","Azenwa","CooL99$","vanessa.azenwa@gmail.com") # create Cresentials object
+        
+
--- user_test.py
+++ user_test.py
@@ -2,7 +2,10 @@ import unittest  #importing the unittest module
 from user import User  #importing the User class
 
 
-
+class TestUser(unittest.TestCase):
+    def setUp(self):
+           
+        self.new_user = User("Vanessa","Azenwa","CooL99$","vanessa.azenwa@gmail.com") # create User object
 
 
 
