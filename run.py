bcc9ce81a0f8295109fa54696b2723fb917641c3
NessaAz <vanessa.azenwa@gmail.com>
2022-04-25 14:53:26 +0300

    define save users and delete user functions

--- run.py
+++ run.py
@@ -11,5 +11,17 @@ def create_user(account_name,user_name,password,email):
     new_user = User(account_name,user_name,password,email)
     return new_user
 
+def save_users(user):
+    '''
+    Function to save users
+    '''
+    user.save_user()    
 
+def del_user(user):
+    '''
+    Function to delete a user account
+    '''
+    user.delete_user()  
+    
+      
 
