368256f31d64a061f51fb09cc7cf37dd438f0ebc
NessaAz <vanessa.azenwa@gmail.com>
2022-04-25 14:27:00 +0300

    create a user account

--- run.py
+++ run.py
@@ -4,4 +4,12 @@ from user import User
 from credentials import Credentials
 import random  #importing random module for generating password
 
+def create_user(account_name,user_name,password,email):
+    '''
+    Function to create a new account
+    '''
+    new_user = User(account_name,user_name,password,email)
+    return new_user
+
+
 
