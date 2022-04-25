3bce27f1ff86cace7f271a1231b946905f72c3b8
NessaAz <vanessa.azenwa@gmail.com>
2022-04-25 07:42:21 +0300

    add the test init method for testing if user object is initialized properly

--- user_test.py
+++ user_test.py
@@ -5,7 +5,17 @@ from user import User  #importing the User class
 class TestUser(unittest.TestCase):
     def setUp(self):
            
-        self.new_user = User("Vanessa","Azenwa","CooL99$","vanessa.azenwa@gmail.com") # create User object
-
+        self.new_user = User("Vanessa Az","Vanessa Azenwa","CooL99$","vanessa.azenwa@gmail.com") # create User object
+        
+    def test_init(self):
+        '''
+        test_init test case to test if the (user) object is initialized properly
+        '''
+        self.assertEqual(self.new_user.user_name,"Vanessa Az")
+        self.assertEqual(self.new_user.user_name,"Vanessa Azenwa")
+        self.assertEqual(self.new_user.password,"CooL99$")
+        self.assertEqual(self.new_user.email,"vanessa.azenwa@gmail.com")    
+        
+        
 
 
