ea7442242d7e06ea928bc2d252d6646119495989
NessaAz <vanessa.azenwa@gmail.com>
2022-04-25 14:16:14 +0300

    add definition to test credentials exist case &find credentials exist case and if__name block

--- credentials_test.py
+++ credentials_test.py
@@ -52,5 +52,31 @@ class TestCredentials(unittest.TestCase):
             self.new_credentials.delete_credentials()# Deleting an account object
             self.assertEqual(len(Credentials.credentials_list),1)              
     
+    def test_find_credentials_by_credentials_name(self):
+        '''
+        test to check if we can find an account by account_name and display information
+        '''
+
+        self.new_credentials.save_credentials()
+        test_credentials = Credentials("Test1","user1","66778899","test1@user.com") 
+        test_credentials.save_credentials()
+
+        found_credentials = Credentials.find_by_name("Test")
+
+        self.assertEqual(found_credentials.email,test_credentials.email)
+        
+    def test_credentials_exists(self):
+        '''
+        test to check if we can return a Boolean  if we cannot find the account.
+        '''
+
+        self.new_credentials.save_credentials()
+        test_credentials = Credentials("Test1","user1","66778899","test1@user.com") 
+        test_credentials.save_credentials()
+
+        credentials_exists = Credentials.credentials_exist("66778899")
+        self.assertTrue(credentials_exists)  
     
 
+if __name__ == '__main__':
+    unittest.main()    
